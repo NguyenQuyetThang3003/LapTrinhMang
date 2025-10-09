12c3e3ded70734d38638e3632f41df5b1092fc07
NguyenQuyetThang3003 <thangnguyen.23092020@gmail.com>
2025-10-09 13:38:56 +0700

    add Dockerfile and render.yaml for Render deploy

--- BlogJavaJS/obj/Debug/net9.0/BlogJavaJS.AssemblyInfo.cs
+++ BlogJavaJS/obj/Debug/net9.0/BlogJavaJS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogJavaJS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469d5187351eeedb57fcf45fefe4adcda7d38082")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ddbb807c6772b2b09d54d429b01c8507c9fc4eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogJavaJS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogJavaJS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
