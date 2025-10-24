ef1c229ba6ec9ada356199d1f778d135a0dfec43
NguyenQuyetThang3003 <thangnguyen.23092020@gmail.com>
2025-10-24 08:35:02 +0700

    up

--- BlogJavaJS/obj/Debug/net8.0/BlogJavaJS.AssemblyInfo.cs
+++ BlogJavaJS/obj/Debug/net8.0/BlogJavaJS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogJavaJS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f7bd9bbe1f51ee67a817439819e42d40755a91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5f02452496e7ea630fedbdfa039fc8e5c6abdf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogJavaJS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogJavaJS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
